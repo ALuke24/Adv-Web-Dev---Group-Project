7a0171aaf668796f7bbfba9b7e9846bb461bf67d
Michelle <98704222+micmeyers@users.noreply.github.com>
2022-11-17 08:17:42 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,144 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["Username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["Username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["Password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["Password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT UserID, Username, password FROM UserID WHERE Username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: main-page.html");
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+<!doctype html>
+<html>
+
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="globalStyle.css">
+</head>
+
+<body>
+<img class='logo'src="EdgewoodCollege.jpg" width="100px" height="100px">
+	<h1 id="title">Login</h1>
+		<div id="navbar"><!--Navigation bar-->
+		<ul>
+			<li><a href="mainpage.html">Homepage</a></li>
+			<li><a href="login.php">Login</a></li>
+			<li><a href="Registstation.php">Registration</a></li>
+			<li><a href="counselinginfo.html">Counseling Info</a></li>
+            <li><a href="staffinfo.html">Staff</a></li>
+            <li><a href="news.html">News</a></li>
+		</ul>
+        </div>
+    <h2>Not Registered?</h2><!--again needs better formatting-->
+    <button class ='button button1'><a class="link" href="Registstation.html">Register</a></button><br>
+    <br>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please user username and password to log in</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="Registstation.php">Sign up now</a>.</p>
+        </form>
+    </div>
+
+</body>
+
+</html>
